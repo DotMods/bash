ba19d8087cf456cfc950087ffdb16cf3cbddf4ee
seanstrom <sean@seanhagstrom.com>
2015-08-12 18:05:12 -0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 # Create symlinks
-ln -s ~/.dotmods/bash/bashconfig ~/.bash_profile
+ln -s ~/.dotmods/bash/bash_profile ~/.bash_profile
 ln -s ~/.dotmods/bash/bashconfig ~/.bashrc
